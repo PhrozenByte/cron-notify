4c259c0560ca147d54c109e3339ecd072b30b0eb
Daniel Rudolf <github.com@daniel-rudolf.de>
2021-09-02 17:12:41 +0200

    Bump to version 1.0.5

--- cron_notify.py
+++ cron_notify.py
@@ -16,7 +16,7 @@ You should have received a copy of the GNU General Public License along with
 this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
-__version__ = "1.0.4"
+__version__ = "1.0.5"
 
 __copyright__ = "Copyright (C) 2016-2021 Daniel Rudolf"
 __license__ = "GPL-3"
